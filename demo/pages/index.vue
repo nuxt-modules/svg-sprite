c544d556e4839eb2d8f55df1e6670206e21205d3
Sébastien Chopin <seb@chopin.io>
2019-02-06 13:00:08 +0100

    demo(add): Add nuxtjs icon

--- demo/pages/index.vue
+++ demo/pages/index.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <div>
+          <svg-icon name="nuxtjs" width="150px" height="150px" />
+        </div>
+        <div>
             <svg-icon name="dreams/zantagle-moon" width="150px" height="150px" />
             <svg-icon name="dreams/Nursery_Unicorn_7946" width="150px" height="150px" />
             <svg-icon name="dreams/Beautiful_Little_Girl_7881" width="150px" height="150px" />
@@ -12,12 +15,6 @@
         </div>
     </div>
 </template>
-
-<script>
-export default {
-
-}
-</script>
 
 <style>
 body {
