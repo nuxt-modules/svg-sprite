1188027be65af5c34d37be27aac8d3aae5bd1bb5
Ahad Birang <farnabaz@gmail.com>
2019-03-22 04:39:40 +0430

    chore(test): add test and circleci config file

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   launch: {
-    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    // to use google chrome, install puppeteer with PUPPETEER_SKIP_CHROMIUM_DOWNLOAD env
+    // and uncomment next line
+    // executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
   }
 }
--- test/module.test.js
+++ test/module.test.js
@@ -12,11 +12,43 @@ afterAll(async () => {
   await nuxt.close()
 })
 
-describe('Test module', () => {
+describe('Render module', () => {
   beforeEach(async () => {
     await page.goto(url('/'))
   })
-  test('true', () => {
-    expect(true).toEqual(true)
+  let hrefs = []
+
+  test('all svg files sould be exist', async () => {
+    hrefs = await page.evaluate(() => {
+      const elements = document.querySelectorAll('use')
+      return Array.from(elements).map(el => el.getAttribute('xlink:href'))
+    })
+    const paths = hrefs.map(path => path.split('#')[0])
+    const sprites = Array.from(new Set(paths))
+
+    for (const sprite of sprites) {
+      const request = await page.goto(url(sprite))
+      expect(request.status()).toEqual(200)
+    }
+  })
+
+  test('all icon should be rendered and have width/height greater than zero', async () => {
+    for (const href of hrefs) {
+      const box = await page.evaluate((href) => {
+        const elements = Array.from(document.querySelectorAll(`use`))
+          .filter(el => el.getAttribute('xlink:href') === href)
+        if (elements.length === 0) {
+          return null
+        }
+        const bbox = elements[0].getBBox()
+        return {
+          width: bbox.width,
+          height: bbox.height
+        }
+      }, href)
+      expect(typeof box).toEqual('object')
+      expect(box.width).toBeGreaterThan(0)
+      expect(box.height).toBeGreaterThan(0)
+    }
   })
 })
