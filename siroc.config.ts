0ee7e7bfe1a46e0ede0ebc06d3249310372fc6bd
Farnabaz <farnabaz@gmail.com>
2021-03-16 15:05:15 +0330

    fix: add svgo plugin to siroc externals

---
+++ siroc.config.ts
@@ -0,0 +1,13 @@
+import { defineSirocConfig } from 'siroc'
+
+export default defineSirocConfig({
+    rollup: {
+        externals: [
+            'svgo/plugins/cleanupIDs',
+            'svgo/plugins/removeAttrs',
+            'svgo/plugins/removeDimensions',
+            'svgo/plugins/removeViewBox',
+            'svgo/plugins/inlineStyles',
+        ]
+    }
+})
