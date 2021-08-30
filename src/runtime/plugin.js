5e8c39619192adf16bd76d0bf732a2c04d0047c1
mrazauskas <72159681+mrazauskas@users.noreply.github.com>
2021-08-30 11:44:26 +0300

    style(plugin): better formatting

--- src/runtime/plugin.js
+++ src/runtime/plugin.js
@@ -1,15 +1,14 @@
 import svgSprite from '~svg-sprite-runtime'
 
 const SPRITES = {
-<%= options.sprites.map(key => `"${key}": () => import('${relativeToBuild(options.output)}/${key}.svg')`).join(',') %>
+  <%= options.sprites.map(key => `'${key}': () => import('${relativeToBuild(options.output)}/${key}.svg')`).join(',\n  ') %>
 }
 
-export default function(context, inject) {
-    inject('svgSprite', svgSprite({
-        importSprite: key => SPRITES[key] ? SPRITES[key]() : Promise.resolve(""),
-        defaultSprite: '<%= options.defaultSprite %>',
-        spriteClassPrefix: '<%= options.spriteClassPrefix %>',
-        spriteClass: '<%= options.elementClass %>',
-    }))
+export default function (_, inject) {
+  inject('svgSprite', svgSprite({
+    importSprite: key => SPRITES[key] ? SPRITES[key]() : Promise.resolve(''),
+    defaultSprite: '<%= options.defaultSprite %>',
+    spriteClassPrefix: '<%= options.spriteClassPrefix %>',
+    spriteClass: '<%= options.elementClass %>'
+  }))
 }
-
